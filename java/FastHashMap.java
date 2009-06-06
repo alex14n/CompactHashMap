a7d0158b7c45eff8edd770b65291b7fe0f243a76
Alex Yakovlev <alex14n@gmail.com>
2009-06-06 20:59:53 +0400

    comments

--- java/FastHashMap.java
+++ java/FastHashMap.java
@@ -2,12 +2,99 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Fast HashMap implementation.
+ * Hash table based implementation of the <tt>Map</tt> interface.  This
+ * implementation provides all of the optional map operations, and permits
+ * <tt>null</tt> values and the <tt>null</tt> key.  (The <tt>HashMap</tt>
+ * class is roughly equivalent to <tt>Hashtable</tt>, except that it is
+ * unsynchronized and permits nulls.)  This class makes no guarantees as to
+ * the order of the map; in particular, it does not guarantee that the order
+ * will remain constant over time.
  *
- * Some benchmark results:
- * http://forums.sun.com/thread.jspa?threadID=5387181
+ * <p>This implementation provides constant-time performance for the basic
+ * operations (<tt>get</tt> and <tt>put</tt>), assuming the hash function
+ * disperses the elements properly among the buckets.  Iteration over
+ * collection views requires time proportional to the "capacity" of the
+ * <tt>HashMap</tt> instance (the number of buckets) plus its size (the number
+ * of key-value mappings).  Thus, it's very important not to set the initial
+ * capacity too high (or the load factor too low) if iteration performance is
+ * important.
  *
+ * <p>An instance of <tt>HashMap</tt> has two parameters that affect its
+ * performance: <i>initial capacity</i> and <i>load factor</i>.  The
+ * <i>capacity</i> is the number of buckets in the hash table, and the initial
+ * capacity is simply the capacity at the time the hash table is created.  The
+ * <i>load factor</i> is a measure of how full the hash table is allowed to
+ * get before its capacity is automatically increased.  When the number of
+ * entries in the hash table exceeds the product of the load factor and the
+ * current capacity, the hash table is <i>rehashed</i> (that is, internal data
+ * structures are rebuilt) so that the hash table has approximately twice the
+ * number of buckets.
+ *
+ * <p>As a general rule, the default load factor (.75) offers a good tradeoff
+ * between time and space costs.  Higher values decrease the space overhead
+ * but increase the lookup cost (reflected in most of the operations of the
+ * <tt>HashMap</tt> class, including <tt>get</tt> and <tt>put</tt>).  The
+ * expected number of entries in the map and its load factor should be taken
+ * into account when setting its initial capacity, so as to minimize the
+ * number of rehash operations.  If the initial capacity is greater
+ * than the maximum number of entries divided by the load factor, no
+ * rehash operations will ever occur.
+ *
+ * <p>If many mappings are to be stored in a <tt>HashMap</tt> instance,
+ * creating it with a sufficiently large capacity will allow the mappings to
+ * be stored more efficiently than letting it perform automatic rehashing as
+ * needed to grow the table.
+ *
+ * <p><strong>Note that this implementation is not synchronized.</strong>
+ * If multiple threads access a hash map concurrently, and at least one of
+ * the threads modifies the map structurally, it <i>must</i> be
+ * synchronized externally.  (A structural modification is any operation
+ * that adds or deletes one or more mappings; merely changing the value
+ * associated with a key that an instance already contains is not a
+ * structural modification.)  This is typically accomplished by
+ * synchronizing on some object that naturally encapsulates the map.
+ *
+ * If no such object exists, the map should be "wrapped" using the
+ * {@link Collections#synchronizedMap Collections.synchronizedMap}
+ * method.  This is best done at creation time, to prevent accidental
+ * unsynchronized access to the map:<pre>
+ *   Map m = Collections.synchronizedMap(new HashMap(...));</pre>
+ *
+ * <p>The iterators returned by all of this class's "collection view methods"
+ * are <i>fail-fast</i>: if the map is structurally modified at any time after
+ * the iterator is created, in any way except through the iterator's own
+ * <tt>remove</tt> method, the iterator will throw a
+ * {@link ConcurrentModificationException}.  Thus, in the face of concurrent
+ * modification, the iterator fails quickly and cleanly, rather than risking
+ * arbitrary, non-deterministic behavior at an undetermined time in the
+ * future.
+ *
+ * <p>Note that the fail-fast behavior of an iterator cannot be guaranteed
+ * as it is, generally speaking, impossible to make any hard guarantees in the
+ * presence of unsynchronized concurrent modification.  Fail-fast iterators
+ * throw <tt>ConcurrentModificationException</tt> on a best-effort basis.
+ * Therefore, it would be wrong to write a program that depended on this
+ * exception for its correctness: <i>the fail-fast behavior of iterators
+ * should be used only to detect bugs.</i>
+ *
+ * <p>This class is a member of the
+ * <a href="{@docRoot}/../technotes/guides/collections/index.html">
+ * Java Collections Framework</a>.
+ *
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ *
+ * @author  Doug Lea
+ * @author  Josh Bloch
+ * @author  Arthur van Hoff
+ * @author  Neal Gafter
  * @author  Alex Yakovlev
+ * @see     Object#hashCode()
+ * @see     Collection
+ * @see     Map
+ * @see     TreeMap
+ * @see     Hashtable
+ * @since   1.2
  */
 public class FastHashMap<K,V>
     extends AbstractMap<K,V>
@@ -62,6 +149,10 @@ public class FastHashMap<K,V>
         // number of collisions (approximately 8 at default load factor).
         return (h >>> 20) ^ (h >>> 12) ^ (h >>> 7) ^ (h >>> 4) ^ h;
     }
+
+    /**
+     * Return improved hash for object o.
+     */
     final static int hash(Object o) {
         int h = o == null ? 0 : o.hashCode();
         return (h >>> 20) ^ (h >>> 12) ^ (h >>> 7) ^ (h >>> 4) ^ h;
@@ -156,6 +247,12 @@ public class FastHashMap<K,V>
     public FastHashMap() {
        this(true);
     }
+
+    /**
+     * Constructor to be used in HashSet
+     * containing only keys without values
+     * thus saving some memory if withValues is false.
+     */
     FastHashMap(boolean withValues) {
         loadFactor = DEFAULT_LOAD_FACTOR;
         hashLen = DEFAULT_INITIAL_CAPACITY;
@@ -178,6 +275,12 @@ public class FastHashMap<K,V>
     public FastHashMap(int initialCapacity, float loadFactor) {
         this(initialCapacity, loadFactor, true);
     }
+
+    /**
+     * Constructor to be used in HashSet
+     * containing only keys without values
+     * thus saving some memory if withValues is false.
+     */
     FastHashMap(int initialCapacity, float loadFactor, boolean withValues) {
         if (initialCapacity < 0)
             throw new IllegalArgumentException(
@@ -210,6 +313,12 @@ public class FastHashMap<K,V>
     public FastHashMap(int initialCapacity) {
         this(initialCapacity, DEFAULT_LOAD_FACTOR, true);
     }
+
+    /**
+     * Constructor to be used in HashSet
+     * containing only keys without values
+     * thus saving some memory if withValues is false.
+     */
     FastHashMap(int initialCapacity, boolean withValues) {
         this(initialCapacity, DEFAULT_LOAD_FACTOR, withValues);
     }
@@ -331,6 +440,12 @@ public class FastHashMap<K,V>
     public V put(K key, V value) {
         return put(key, value, true);
     }
+
+    /**
+     * Create a new key/value mapping without looking
+     * if such key is already present in this map
+     * if searchForExistingKey is false.
+     */
     @SuppressWarnings("unchecked")
     final V put(K key, V value, boolean searchForExistingKey) {
         int hc = hash(key);
@@ -472,8 +587,8 @@ public class FastHashMap<K,V>
     }
 
     /**
-     * Returns a shallow copy of this <tt>HashMap</tt> instance: the keys and
-     * values themselves are not cloned.
+     * Returns a shallow copy of this <tt>HashMap</tt> instance:
+     * the keys and values themselves are not cloned.
      *
      * @return a shallow copy of this map
      */
@@ -794,8 +909,8 @@ public class FastHashMap<K,V>
     }
 
     /**
-     * Save the state of the <tt>HashMap</tt> instance to a stream (i.e.,
-     * serialize it).
+     * Save the state of the <tt>HashMap</tt> instance
+     * to a stream (i.e., serialize it).
      *
      * @serialData The <i>capacity</i> of the HashMap (the length of the
      *             bucket array) is emitted (int), followed by the
@@ -826,8 +941,8 @@ public class FastHashMap<K,V>
     private static final long serialVersionUID = 362498820763181265L;
 
     /**
-     * Reconstitute the <tt>HashMap</tt> instance from a stream (i.e.,
-     * deserialize it).
+     * Reconstitute the <tt>HashMap</tt> instance
+     * from a stream (i.e., deserialize it).
      */
     private void readObject(ObjectInputStream s)
          throws IOException, ClassNotFoundException
@@ -857,18 +972,7 @@ public class FastHashMap<K,V>
         }
     }
 
-    // These methods are used when serializing HashSets
-    int   capacity()     { return hashLen; }
-    float loadFactor()   { return loadFactor; }
-
-    // These hooks are needed for LinkedHashMap
-    void addHook(int i) { }
-    void updateHook(int i) { }
-    void removeHook(int i) { }
-
-    /**
-     *
-     */
+    // ToDo: check modCount and throw IllegalStateException?
     @SuppressWarnings("unchecked")
     final class Entry implements Map.Entry<K,V> {
         private final int index;
@@ -911,4 +1015,13 @@ public class FastHashMap<K,V>
             return getKey() + "=" + getValue();
         }
     }
+
+    // These methods are used when serializing HashSets
+    int   capacity()     { return hashLen; }
+    float loadFactor()   { return loadFactor; }
+
+    // These hooks are needed for LinkedHashMap
+    void addHook(int i) { }
+    void updateHook(int i) { }
+    void removeHook(int i) { }
 }
